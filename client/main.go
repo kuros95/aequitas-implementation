d73e7beff3dcba5b8fa965105add49c159f56de2
Kurosinho <marcin.kur95@gmail.com>
2024-06-03 21:40:48 +0200

    working grpc client and server

---
+++ client/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+	"magisterium/stayalive"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:2222", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to gRPC server at localhost:2222: %v", err)
+	}
+	defer conn.Close()
+	c := stayalive.NewStayAliveServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := c.StayAlive(ctx, &stayalive.StayAliveRequest{})
+	if err != nil {
+		log.Fatalf("error calling function SayHello: %v", err)
+	}
+
+	log.Printf("gRPC server response: %v", r.GetAliveResp())
+}
