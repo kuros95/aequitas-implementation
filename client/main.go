0648e82e0b5f5f8db8aa9e86d10aac8f36cd90f1
Marcin Kurowski <01149547@pw.edu.pl>
2024-06-10 13:47:19 +0200

    hardcoded priority reduction working

--- client/main.go
+++ client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	aeuqitas "magisterium/aequitas"
 	"magisterium/stayalive"
 	"time"
 
@@ -10,7 +11,7 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func main() {
+func connect(prio int) time.Duration {
 	conn, err := grpc.Dial("localhost:2222", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("failed to connect to gRPC server at localhost:2222: %v", err)
@@ -27,6 +28,20 @@ func main() {
 		log.Fatalf("error calling function SayHello: %v", err)
 	}
 	elapsed := time.Since(start)
+	log.Printf("gRPC server response: %v, with elapsed time: %v, and priority: %v", r.GetAliveResp(), elapsed, prio)
 
-	log.Printf("gRPC server response: %v, with elapsed time: %v", r.GetAliveResp(), elapsed)
+	return elapsed
+}
+
+func main() {
+	prio := 3
+	for {
+		elapsed := connect(prio)
+		reduce := aeuqitas.TimeCheck(time.Millisecond, elapsed)
+		if reduce && prio > 0 {
+			prio = prio - 1
+		}
+		time.Sleep(time.Second)
+	}
+
 }
