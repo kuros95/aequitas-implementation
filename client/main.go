8eb9f18461032a561db05a61d92c5f8f6439039b
Kurosinho <marcin.kur95@gmail.com>
2024-10-29 12:29:20 +0100

    rpcLo implemented, waiting for testing

--- client/main.go
+++ client/main.go
@@ -9,10 +9,11 @@ import (
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/internal/metadata"
 )
 
 func connect(prio int) time.Duration {
-	conn, err := grpc.Dial("localhost:2222", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	conn, err := grpc.NewClient("localhost:2222", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("failed to connect to gRPC server at localhost:2222: %v", err)
 	}
@@ -32,6 +33,36 @@ func connect(prio int) time.Duration {
 
 	return elapsed
 }
+
+func rpcLo() bool {
+	conn, err := grpc.NewClient("localhost:2222", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("failed to connect to gRPC server at localhost:2222: %v", err)
+		return false
+	}
+	defer conn.Close()
+	c := stayalive.NewStayAliveServiceClient(conn)
+
+	header := metadata.New(map[string]string{
+		"prio": "lo",
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	ctxWithMD := metadata.NewContext(ctx, header)
+
+	resp, err := c.StayAlive(ctxWithMD, &stayalive.StayAliveRequest{})
+	if err != nil {
+		log.Fatalf("error calling function StayAlive: %v", err)
+	}
+
+	return resp.GetAliveResp()
+}
+
+func rpcHi()
+
+func rpcBe()
 
 func main() {
 	prio := 3
