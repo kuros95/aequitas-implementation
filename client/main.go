713ca54d175b08c02c646c71828fe1e8a1b1a042
Marcin Kurowski <01149547@pw.edu.pl>
2024-11-02 12:28:20 +0100

    rpc barebones working

--- client/main.go
+++ client/main.go
@@ -2,43 +2,70 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"magisterium/aequitas"
 	"magisterium/stayalive"
 	"time"
 
+	"golang.org/x/exp/rand"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 )
 
-func connect(prio int) time.Duration {
-	conn, err := grpc.Dial("localhost:2222", grpc.WithTransportCredentials(insecure.NewCredentials()))
+func rpcWithPrio(prio string, lowered bool) bool {
+	if lowered {
+		prio = "be"
+	}
+	conn, err := grpc.NewClient("localhost:2222", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("failed to connect to gRPC server at localhost:2222: %v", err)
+		return false
 	}
 	defer conn.Close()
 	c := stayalive.NewStayAliveServiceClient(conn)
 
+	header := metadata.New(map[string]string{
+		"prio": prio,
+	})
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	start := time.Now()
-	r, err := c.StayAlive(ctx, &stayalive.StayAliveRequest{})
-	if err != nil {
-		log.Fatalf("error calling function SayHello: %v", err)
-	}
-	elapsed := time.Since(start)
-	log.Printf("gRPC server response: %v, with elapsed time: %v, and priority: %v", r.GetAliveResp(), elapsed, prio)
+	ctxWithMD := metadata.NewOutgoingContext(ctx, header)
 
-	return elapsed
+	resp, err := c.StayAlive(ctxWithMD, &stayalive.StayAliveRequest{})
+	fmt.Printf("sending RPC with priority: %v \n", prio)
+	if err != nil {
+		log.Fatalf("error calling function StayAlive: %v", err)
+	}
+
+	return resp.GetAliveResp()
 }
 
 func main() {
-	prio := 3
+	prios := [2]string{"hi", "lo"}
+	var done int
+	var fail int
+	lowered := false
+	//send rpcs in parallel with different prios, then measure each individually and individually lower prio, retain lowered prio
+	//needs a category of rpc to send, so that lowered prio may be preserved (interface?), and they may be differently served
+	//weighted random selection of priorities required
 	for {
-		elapsed := connect(prio)
+		prio := prios[rand.Intn(len(prios))]
+		start := time.Now()
+		completed := rpcWithPrio(prio, lowered)
+		elapsed := time.Since(start)
+		if completed {
+			done++
+		} else {
+			fail++
+		}
+
 		reduce := aequitas.TimeCheck(time.Millisecond, elapsed)
-		if reduce && prio > 0 {
-			prio = prio - 1
+		if reduce && prio == "hi" || prio == "lo" {
+			lowered = true
 		}
 		time.Sleep(time.Second)
 	}
