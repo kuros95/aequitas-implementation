b0f66daeeecb8407a24b359c2aaa833f33a39ad2
kuros95 <marcin.kur95@gmail.com>
2024-11-28 14:49:45 +0000

    HTB corrections and kube deployment drafts

--- tc-on-host.sh
+++ tc-on-host.sh
@@ -6,11 +6,11 @@ src_ip=$(ip addr show eth0 scope global | grep 172 | awk '{print $2}')
 echo "adding traffic control on eth0 interface and source ip" $src_ip
 
 tc qdisc add dev eth0 root handle 1: htb default 4 direct_qlen 100000
-tc class add dev eth0 parent 1: classid 1:1 htb rate 1000mbps ceil 1000mbps
+tc class add dev eth0 parent 1: classid 1:1 htb rate 2000mbps ceil 2000mbps
 
-tc class add dev eth0 parent 1:1 classid 1:2 htb rate 200mbps ceil 1000mbps
-tc class add dev eth0 parent 1:1 classid 1:3 htb rate 700mbps ceil 1000mbps
-tc class add dev eth0 parent 1:1 classid 1:4 htb rate 100mbps ceil 1000mbps
+tc class add dev eth0 parent 1:1 classid 1:2 htb rate 400mbps ceil 2000mbps
+tc class add dev eth0 parent 1:1 classid 1:3 htb rate 1400mbps ceil 2000mbps
+tc class add dev eth0 parent 1:1 classid 1:4 htb rate 200mbps ceil 2000mbps
 
 tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip src $src_ip \
         match ip dport 2220 0xffff flowid 1:2
