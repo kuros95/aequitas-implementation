38f17fd24aef18626a8522053667bf8a0a968f58
kuros95 <marcin.kur95@gmail.com>
2025-08-12 07:16:31 +0000

    important details

--- tests/tc-on-host.sh
+++ tests/tc-on-host.sh
@@ -7,14 +7,14 @@ echo "adding traffic control on eth0 interface and source ip" $src_ip
 
 tc qdisc del dev eth0 root 2>/dev/null
 tc qdisc add dev eth0 root handle 1: htb default 4 direct_qlen 100
-tc class add dev eth0 parent 1: classid 1:1 htb rate 15mbps ceil 15mbps
+tc class add dev eth0 parent 1: classid 1:1 htb rate 10mbps ceil 10mbps
 
-tc class add dev eth0 parent 1:1 classid 1:2 htb rate 5mbps ceil 15mbps
-tc class add dev eth0 parent 1:1 classid 1:3 htb rate 7mbps ceil 15mbps
-tc class add dev eth0 parent 1:1 classid 1:4 htb rate 0mbps ceil 15mbps
+tc class add dev eth0 parent 1:1 classid 1:2 htb rate 3mbps ceil 10mbps
+tc class add dev eth0 parent 1:1 classid 1:3 htb rate 5mbps ceil 10mbps
+tc class add dev eth0 parent 1:1 classid 1:4 htb rate 0mbps ceil 10mbps
 
-tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dsfield 0x40 0xfc classid 1:2
-tc filter add dev eth0 protocol ip parent 1: prio 2 u32 match ip dsfield 0x20 0xfc classid 1:3
+tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dsfield 0x40 0x1e classid 1:2
+tc filter add dev eth0 protocol ip parent 1: prio 2 u32 match ip dsfield 0x20 0x1e classid 1:3
 
 tc qdisc add dev eth0 parent 1:2 handle 12: sfq perturb 10
 tc qdisc add dev eth0 parent 1:3 handle 13: sfq perturb 10
@@ -24,10 +24,6 @@ echo "traffic control added"
 
 # The int value for 0x20 is 32, and for 0x40 is 64.
 #10 for 1, 10 for 10, 10 for 100, 10 for 1000
-#10 for 32KB, 15 for 64KB
-
-#for u-1 it was 15 5/7/0 minimum
-#for u-10 it was 20 6/10/0 minimum
-#for u-100 it was 10 3/5/0 minimum
-#for u-1000 it was 5 1/2/0 minimum
-
+#for 30 it was 10/15/0 minimum
+#for 35 it was 12/18/0 minimum
+#for 40 it was 13/20/0 minimum
